9df63b34dccb4a4b690250e8dd4d8339338e208d
ErikGson <erkijes@gmail.com>
2025-05-23 16:45:42 +0200

    Nya länkar i header

--- header.php
+++ header.php
@@ -9,19 +9,19 @@ if (session_status() == PHP_SESSION_NONE) {
     <div class="logo">Cryptotracker</div> <!-- logotyp -->
     <nav>
         <ul>
-            <li><a href="/Cryptotracker/index.php">Hem</a></li>
-            <li><a href="/Cryptotracker/pages/dashboard.php">Dashboard</a></li>
-            <li><a href="/Cryptotracker/pages/portfolio.php">Portfölj</a></li>
-            <li><a href="/Cryptotracker/pages/search.php">Sök</a></li>
+            <li><a href="/Crypto-Tracker/index.php">Hem</a></li>
+            <li><a href="/Crypto-Tracker/pages/dashboard.php">Dashboard</a></li>
+            <li><a href="/Crypto-Tracker/pages/portfolio.php">Portfölj</a></li>
+            <li><a href="/Crypto-Tracker/pages/search.php">Sök</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
 
                 <!-- Visas enbart när användare är inloggad -->
-                <li><a href="/Cryptotracker/pages/logout.php">Logga ut</a></li>
+                <li><a href="/Crypto-Tracker/pages/logout.php">Logga ut</a></li>
             <?php else: ?>
                 
                 <!-- Visas om ingen är inloggad -->
-                <li><a href="/Cryptotracker/pages/login.php">Logga in</a></li>
-                <li><a href="/Cryptotracker/pages/register.php">Registrera</a></li>
+                <li><a href="/Crypto-Tracker/pages/login.php">Logga in</a></li>
+                <li><a href="/Crypto-Tracker/pages/register.php">Registrera</a></li>
             <?php endif; ?>
         </ul>
     </nav>
