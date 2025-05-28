84bf6a7fac3919c8281d1e1223fa4326193626f3
ErikGson <erkijes@gmail.com>
2025-05-28 18:29:52 +0200

    småändring i index.php

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
   <meta charset="UTF-8">
   <title>Cryptotracker</title>
   <!-- Länk: CSS -->
   <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
   <?php include 'header.php'; ?> <!-- header: nav -->
-  
-  <div class="container">
+
+  <?php if (!isset($_SESSION["user_id"])) { ?>
+    <div class="container">
       <h1>Välkommen till Cryptotracker</h1> <!-- rubrik -->
       <p>En plattform för att följa och hantera kryptovalutor.</p> <!-- beskrivning -->
       <div class="cta-buttons"> <!-- knappar -->
-          <a href="pages/register.php" class="btn">Skapa Konto</a>
-          <a href="pages/login.php" class="btn">Logga In</a>
+        <a href="pages/register.php" class="btn">Skapa Konto</a>
+        <a href="pages/login.php" class="btn">Logga In</a>
       </div>
-  </div>
-  
+    </div>
+    
+    <?php } else { ?>
+    <div class="container">
+      <h1>Välkommen till Cryptotracker</h1> <!-- rubrik -->
+      <p>Du är nu inloggad!</p>
+      <?php } ?>
+</div>
+
+
   <?php include 'php/footer.php'; ?> <!-- footer -->
 </body>
+
 </html>
